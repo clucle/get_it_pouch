56d67a2a3681bed2237ccf562c0bf8032e18cc08
Junseok Oh <kotohana5706@edcan.kr>
2017-05-14 08:34:40 +0900

    Implement SettingsActivity

--- app/src/main/java/kr/edcan/getitpouch/fragment/SettingsFragment.java
+++ app/src/main/java/kr/edcan/getitpouch/fragment/SettingsFragment.java
@@ -1,7 +1,22 @@
 package kr.edcan.getitpouch.fragment;
 
 import android.content.Context;
+import android.databinding.ViewDataBinding;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.android.databinding.library.baseAdapters.BR;
+import com.github.nitrico.lastadapter.ItemType;
+import com.github.nitrico.lastadapter.LastAdapter;
+import com.github.nitrico.lastadapter.LayoutHandler;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import kr.edcan.getitpouch.R;
+import kr.edcan.getitpouch.databinding.ContentSettingsBinding;
 import kr.edcan.getitpouch.databinding.FragmentSettingsBinding;
 
 /**
@@ -11,9 +26,32 @@ import kr.edcan.getitpouch.databinding.FragmentSettingsBinding;
 public class SettingsFragment {
     private Context context;
     private FragmentSettingsBinding fragmentSettingsBinding;
+    private RecyclerView settingsRecyclerView;
+    private ArrayList<String> dataList = new ArrayList<>();
 
     public SettingsFragment(Context context, FragmentSettingsBinding fragmentSettingsBinding) {
         this.context = context;
         this.fragmentSettingsBinding = fragmentSettingsBinding;
+        setDefault();
+    }
+
+    private void setDefault() {
+        settingsRecyclerView = fragmentSettingsBinding.settingsRecyclerView;
+        settingsRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        setData();
+        LastAdapter.with(dataList, BR.content)
+                .map(String.class, new ItemType<ContentSettingsBinding>(R.layout.content_settings))
+                .handler(new LayoutHandler() {
+                    @Override
+                    public int getItemLayout(@NotNull Object o, int i) {
+                        return R.layout.content_settings;
+                    }
+                })
+                .into(settingsRecyclerView);
+    }
+
+    private void setData() {
+        Collections.addAll(dataList,
+                "계정설정", "푸시알림", "공지사항", "이용약관", "개인정보 취급방침");
     }
 }
